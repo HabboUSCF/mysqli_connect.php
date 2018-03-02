3b2459bef48e7c6589ea10592a87f1f8ee68723f
HabboUSCF <36978387+HabboUSCF@users.noreply.github.com>
2018-03-01 22:10:57 -0600

    Add files via upload

---
+++ addpersonnel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Add Personnel</title>
+	</head>
+	<body>
+		
+		<form action="http://localhost/....php" method="post">
+		    
+		    <b>PTS PROFILE FORM</b>
+		    
+		    <p>USERNAME:
+		    <input type="text" name="username" size="30" value="" /></p>
+		    
+		    <p>RANK:
+		    <input type="text" name="rank" size="30" value="" /></p>
+		    
+		    <p>MOTTO:
+		    <input type="text" name="motto" size="30" value="" /></p>
+		    
+		    <p>DATE OF ENLISTMENT:
+		    <input type="text" name="date_joined" size="30" value="" /></p>
+		    
+		    <p>
+		        <input type="submit" name="submit" value="SEND" />
+		    </p>
+		</form>
+		
+	</body>
+</html>

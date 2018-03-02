a23fba1aa761ca23133bcebd26e66294fbd713c2
HabboUSCF <36978387+HabboUSCF@users.noreply.github.com>
2018-03-01 22:50:42 -0600

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_USER', '');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'USCF PTS');
+DEFINE ('DB_USER', 'newuser');
+DEFINE ('DB_PASSWORD', 'Bricenasa1');
+DEFINE ('DB_HOST', 'github');
+DEFINE ('DB_NAME', 'USCF PTS (sakila)');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could Not Connect to MySQL' .

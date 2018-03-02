542a3b6426ae63a19349de6e73ab1aa6ff0f1386
HabboUSCF <36978387+HabboUSCF@users.noreply.github.com>
2018-03-01 22:55:35 -0600

    Update addpersonnel.php

--- addpersonnel.php
+++ addpersonnel.php
@@ -5,21 +5,21 @@
 	</head>
 	<body>
 		
-		<form action="http://localhost/....php" method="post">
+		<form action="mysqli_connect.php/personneladded.php" method="post">
 		    
 		    <b>PTS PROFILE FORM</b>
 		    
 		    <p>USERNAME:
-		    <input type="text" name="username" size="30" value="" /></p>
+		    <input type="text" name="username" size="30" value="" placeholder="FULL USERNAME" /></p>
 		    
 		    <p>RANK:
-		    <input type="text" name="rank" size="30" value="" /></p>
+		    <input type="text" name="rank" size="30" value="" placeholder="FULL RANK" /></p>
 		    
 		    <p>MOTTO:
-		    <input type="text" name="motto" size="30" value="" /></p>
+		    <input type="text" name="motto" size="30" value="" placeholder="[BRANCH] RANK/OFFICE {ADTs}" /></p>
 		    
 		    <p>DATE OF ENLISTMENT:
-		    <input type="text" name="date_joined" size="30" value="" /></p>
+		    <input type="text" name="date_joined" size="30" value="" placeholder="YY/MM/DD" /></p>
 		    
 		    <p>
 		        <input type="submit" name="submit" value="SEND" />
